a2f1f6e53336e85c5feac2fc2d1003fea1099bd6
Harrison Goldstein <hgoldstein95@gmail.com>
2022-12-07 11:10:27 -0500

    Add generator for day 7

--- day7/Day7.ml
+++ day7/Day7.ml
@@ -1,5 +1,6 @@
 open! Core
 open! Stdio
+module G = Core.Quickcheck.Generator
 
 let example =
   "$ cd /\n\
@@ -26,46 +27,82 @@ let example =
    5626152 d.ext\n\
    7214296 k"
 
-type fs_line = Cd of string | Ls | File of int * string | Dir of string
+type fname = (string[@quickcheck.generator String.gen_nonempty' Char.gen_alpha])
 [@@deriving equal, compare, sexp_of, quickcheck]
 
-let fs_line_to_string = function
-  | Cd dir -> sprintf "$ cd %s" dir
-  | Ls -> "$ ls"
-  | File (size, name) -> sprintf "%d %s" size name
-  | Dir name -> sprintf "dir %s" name
+module TermLine : sig
+  type t = Cd of string | Ls | File of int * string | Dir of string
+  [@@deriving equal, compare, sexp_of, quickcheck]
 
-let parse_line s =
-  match String.split s ~on:' ' with
-  | [ "$"; "cd"; dir ] -> Cd dir
-  | [ "$"; "ls" ] -> Ls
-  | [ "dir"; dirname ] -> Dir dirname
-  | [ size; name ] -> File (Int.of_string size, name)
-  | _ -> failwithf "Invalid line: %s" s ()
+  val to_string : t -> string
+  val of_string : string -> t
+end = struct
+  type t = Cd of fname | Ls | File of int * fname | Dir of fname
+  [@@deriving equal, compare, sexp_of, quickcheck]
 
-let%expect_test "parse_line" =
-  print_s [%sexp (parse_line "$ cd /" : fs_line)];
-  [%expect {| (Cd /) |}];
-  print_s [%sexp (parse_line "$ ls" : fs_line)];
-  [%expect {| Ls |}];
-  print_s [%sexp (parse_line "10394 foo.txt" : fs_line)];
-  [%expect {| (File 10394 foo.txt) |}];
-  print_s [%sexp (parse_line "10394 xyz" : fs_line)];
-  [%expect {| (File 10394 xyz) |}];
-  print_s [%sexp (parse_line "dir bar" : fs_line)];
-  [%expect {| (Dir bar) |}]
+  let to_string = function
+    | Cd dir -> sprintf "$ cd %s" dir
+    | Ls -> "$ ls"
+    | File (size, name) -> sprintf "%d %s" size name
+    | Dir name -> sprintf "dir %s" name
 
-let%test_unit "parse_line roundtrip" =
-  example |> String.split_lines |> List.map ~f:parse_line
-  |> List.map ~f:fs_line_to_string
+  let of_string s =
+    match String.split s ~on:' ' with
+    | [ "$"; "cd"; dir ] -> Cd dir
+    | [ "$"; "ls" ] -> Ls
+    | [ "dir"; dirname ] -> Dir dirname
+    | [ size; name ] -> File (Int.of_string size, name)
+    | _ -> failwithf "Invalid line: %s" s ()
+
+  let%expect_test "to_string" =
+    print_s [%sexp (of_string "$ cd /" : t)];
+    [%expect {| (Cd /) |}];
+    print_s [%sexp (of_string "$ ls" : t)];
+    [%expect {| Ls |}];
+    print_s [%sexp (of_string "10394 foo.txt" : t)];
+    [%expect {| (File 10394 foo.txt) |}];
+    print_s [%sexp (of_string "10394 xyz" : t)];
+    [%expect {| (File 10394 xyz) |}];
+    print_s [%sexp (of_string "dir bar" : t)];
+    [%expect {| (Dir bar) |}]
+
+  let%test_unit "roundtrip" =
+    Quickcheck.test [%quickcheck.generator: t] ~sexp_of:[%sexp_of: t]
+      ~shrinker:[%quickcheck.shrinker: t] ~f:(fun line ->
+        [%test_eq: t] (of_string (to_string line)) line)
+end
+
+let%test_unit "TermLine roundtrip" =
+  example |> String.split_lines
+  |> List.map ~f:TermLine.of_string
+  |> List.map ~f:TermLine.to_string
   |> String.concat ~sep:"\n"
   |> [%test_eq: string] example
 
-(** TODO Fix generator *)
-let%test_unit "parse_line roundtrip qc" =
-  Quickcheck.test [%quickcheck.generator: fs_line] ~sexp_of:[%sexp_of: fs_line]
-    ~f:(fun line ->
-      [%test_eq: fs_line] (parse_line (fs_line_to_string line)) line)
+module FileSystem = struct
+  type t = File of fname | Directory of (fname * t) list
+  [@@deriving equal, compare, sexp_of, variants]
+
+  let quickcheck_generator =
+    let open G.Let_syntax in
+    let g_fname = G.map ~f:file [%quickcheck.generator: fname] in
+    let rec aux n =
+      if n = 0 then g_fname
+      else
+        G.weighted_union
+          [
+            (1., g_fname);
+            ( 5.,
+              G.map ~f:directory
+                (G.list_non_empty
+                   (let%bind f = [%quickcheck.generator: fname] in
+                    let%map x = aux (n - 1) in
+                    (f, x))) );
+          ]
+    in
+    let%map x = aux 9 in
+    ("/", [ x ])
+end
 
 let part1 _ = ""
 
